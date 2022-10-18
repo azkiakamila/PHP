2051f74922b4cd8ac5b2c66646aab690013b8a23
azkiakamila <90243646+azkiakamila@users.noreply.github.com>
2022-10-19 00:59:10 +0700

    Add files via upload

---
+++ Pemrograman Berorientasi Objek/Praktikum 6/No2/interface.php
@@ -0,0 +1,14 @@
+<--- Azkia Kamila ; 21091397027 ; 2021A --->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
